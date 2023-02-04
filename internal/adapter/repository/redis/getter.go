a87358d6dcf025e808ecedc6216dd6f97852db40
blarth <jminocente@gmail.com>
2023-02-04 01:18:28 -0300

    feat: redis

---
+++ internal/adapter/repository/redis/getter.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+func getter() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	id, err := client.Get("api:match:1").Int64()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("match id: ", id)
+
+	// Output: match id: 1
+}
