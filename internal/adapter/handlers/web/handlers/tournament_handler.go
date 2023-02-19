ce99ec0b410bb86ed72f0974141f7ce7fc0cdc1f
blarth <jminocente@gmail.com>
2023-02-19 17:23:42 -0300

    feat: hexagonal arch

---
+++ internal/adapter/handlers/web/handlers/tournament_handler.go
@@ -0,0 +1,23 @@
+package web
+
+import "github.com/gin-gonic/gin"
+
+type HTTPHandler struct {
+	gamesService ports.GamesService
+}
+
+func NewHTTPHandler(gamesService ports.GamesService) *HTTPHandler {
+	return &HTTPHandler{
+		gamesService: gamesService,
+	}
+}
+
+func (hdl *HTTPHandler) Get(c *gin.Context) {
+	game, err := hdl.gamesService.Get(c.Param("id"))
+	if err != nil {
+		c.AbortWithStatusJSON(500, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.JSON(200, game)
+}
---
+++ internal/core/domain/model/tournament/tournament.go
@@ -0,0 +1,51 @@
+package domain
+
+import (
+	"errors"
+	"time"
+
+	domain "github.com/duel-me-next-level/api-consumer/internal/core/domain/model/league"
+)
+
+type Tournament struct {
+	BeginAt       time.Time
+	EndAt         time.Time
+	ID            int32
+	League        *domain.League
+	VideoGameID   int32
+	videoGameName string
+	winnerID      int32
+	winnerType    string
+}
+
+func NewTournament(beginAt time.Time, endAt time.Time, id int32, league domain.League, videoGameID int32, winnerId int32, winnerType string) *Tournament {
+
+	videoGameName, err := getVideoGameName(videoGameID)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Tournament{
+		BeginAt:       beginAt,
+		EndAt:         endAt,
+		ID:            id,
+		League:        domain.NewLeague(league.LeagueID, league.LiveSupported),
+		VideoGameID:   videoGameID,
+		videoGameName: videoGameName,
+		winnerID:      winnerId,
+		winnerType:    winnerType,
+	}
+}
+
+func getVideoGameName(videoGameID int32) (string, error) {
+	videoGameMap := map[int32]string{
+		1: "LoL",
+		3: "CS:GO",
+		4: "Dota 2",
+	}
+	videoGameName, err := videoGameMap[videoGameID]
+	if !err {
+		return "", errors.New("video game not found")
+	}
+	return videoGameName, nil
+}
