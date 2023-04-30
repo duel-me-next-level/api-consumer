1e6f746aa6e8cc9d0f0a7a07c686a9840b3dd406
blarth <jminocente@gmail.com>
2023-04-30 13:50:30 -0300

    fix: service interface

--- internal/core/domain/ports/tournament_service.go
+++ internal/core/domain/ports/tournament_service.go
@@ -1,12 +1,12 @@
-package tournament_interface
+package repository
 
 import (
 	"time"
 
-	domain "github.com/duel-me-next-level/api-consumer/internal/core/domain/model/tournament"
+	domain "github.com/duel-me-next-level/api-consumer/internal/core/domain/model"
 )
 
 type TournamentService interface {
 	GetAll() (domain.Tournament, error)
-	Create(BeginAt time.Time, EndAt time.Time, LeagueID int) (domain.Tournament, error)
+	Create(BeginAt time.Time, EndAt time.Time, League domain.League) (domain.Tournament, error)
 }
